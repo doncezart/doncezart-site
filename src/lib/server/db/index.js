232307afde50d127fe31d8bb170955c0404725ab
cezar <goldie.czr@gmail.com>
2026-06-01 04:04:18 +0300

    fix(db): treat LAN IPs as local and support DB_SSL override

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -10,8 +10,9 @@ let _db = null;
 function getDb() {
 	if (!_db) {
 		if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-		const isLocal = /(?:@|\/\/)(?:localhost|127\.0\.0\.1|::1)(?::|\/|$)/.test(env.DATABASE_URL);
-		_db = drizzle(postgres(env.DATABASE_URL, isLocal ? {} : { ssl: 'require' }));
+		const isLocal = /(?:@|\/\/)(?:localhost|127\.0\.0\.1|::1|10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|192\.168\.[0-9]{1,3}\.[0-9]{1,3}|172\.(?:1[6-9]|2[0-9]|3[0-1])\.[0-9]{1,3}\.[0-9]{1,3})(?::|\/|$)/.test(env.DATABASE_URL);
+		const useSsl = env.DB_SSL?.toLowerCase() !== 'false' && !isLocal;
+		_db = drizzle(postgres(env.DATABASE_URL, useSsl ? { ssl: 'require' } : {}));
 	}
 	return _db;
 }
